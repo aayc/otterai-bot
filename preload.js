e2d52a36c7c3aba0beb45c41cfdfca648e82d3fd
Aaron Chan <aaronchan@Aarons-MBP.attlocal.net>
2022-07-31 15:48:11 -0700

    Add uploading dialogues

--- preload.js
+++ preload.js
@@ -1,12 +1,47 @@
+const { contextBridge, ipcRenderer } = require('electron')
+const moment = require("moment")
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
+function readVoicemailFiles() {
+  const directoryPath = process.env.UNPROCESSED_VOICEMAILS_FOLDER
+  const files = fs.readdirSync(directoryPath)
+                  .filter(fname => !fs.lstatSync(directoryPath + "/" + fname).isDirectory())
+                  .filter(fname => [".mp3", ".wav", ".m4a"].some(ext => path.extname(fname).endsWith(ext)))
+                  .map(fname => directoryPath + "/" + fname);
+
+  if (!fs.existsSync(process.env.UNPROCESSED_VOICEMAILS_FOLDER + "/processing")) {
+      fs.mkdirSync(process.env.UNPROCESSED_VOICEMAILS_FOLDER + "/processing");
+  }
+
+  let id = 1
+  const newFilePaths = []
+  for (const file of files) {
+      const stats = fs.statSync(file)
+      const createdAt = moment(stats.birthtime).format("YYYY-MM-DD")
+      const newFileName = createdAt + "-" + id + path.extname(file)
+      const newFilePath = process.env.UNPROCESSED_VOICEMAILS_FOLDER + "/processing/" + newFileName
+      fs.copyFileSync(file, newFilePath)
+      id += 1
+      newFilePaths.push(newFilePath)
+  }
+  return newFilePaths
+}
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+  const fileNames = readVoicemailFiles()
+  document.getElementById("fileList").innerHTML = fileNames.map(f => `<li>${f}</li>`).join("")
+  document.getElementById("fileList").data = fileNames
 })
+
+
+contextBridge.exposeInMainWorld('electron',
+    {
+        send: (channel, payload) => ipcRenderer.invoke(channel, payload)
+    }
+)
+
--- renderer.js
+++ renderer.js
@@ -1,6 +1,25 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+let loading = false
+
+uploadButton.onclick = async function () {
+    if (!loading) {
+        loading = true
+        uploadButton.innerHTML = "Uploading..."
+
+        const fileList = document.getElementById("fileList").innerHTML.split("</li><li>").map(s => s.replaceAll("<li>", "").replaceAll("</li>", ""))
+        const { error, result } = await window.electron.send("upload", { fileList })
+
+        loading = false
+        uploadButton.innerHTML = "Upload"
+
+        if (error) {
+            alert("err")
+            alert(error)
+            return
+        }
+
+        alert("Uploaded files to otter.ai:" + result.join(", "))
+        const newFileList = fileList.filter(f => !result.includes(f))
+        document.getElementById("fileList").innerHTML = newFileList.map(f => `<li>${f}</li>`).join("")
+        document.getElementById("successFileList").innerHTML = result.map(f => `<li>${f}</li>`).join("")
+    }
+}
